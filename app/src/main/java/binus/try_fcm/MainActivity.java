7c1b67d4091afe298612b7fbdf4cafe0e795e9c6
Timothy Yudi <timothyyudi@gmail.com>
2017-03-02 20:16:57 +0700

    Initial commit

---
+++ app/src/main/java/binus/try_fcm/MainActivity.java
@@ -0,0 +1,17 @@
+package binus.try_fcm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this,MyFirebaseInstanceIDService.class));
+        startService(new Intent(this,MyFirebaseMessagingService.class));
+    }
+}
---
+++ app/src/main/java/binus/try_fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,29 @@
+package binus.try_fcm;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by root on 3/2/2017.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    final String TAG = "fbInstanceIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+    }
+}
